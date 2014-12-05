e3a3bd20d8d6152fa03e9e90f8e20c3769cec088
superjohnny <jcg@angryyak.co.uk>
2014-12-05 13:08:17 +0000

    add i2c

--- bluepresence.sh
+++ bluepresence.sh
@@ -9,31 +9,44 @@ rssi=-1
 state=0
 
 #Command loop:
-while [ 1 ]; do
-cmdout=$(l2ping $device -c 1)
-btcurrent=$(echo $cmdout | grep -c "RSSI return value") 2> /dev/null
-rssi=$(echo $cmdout | sed -e 's/RSSI return value: //g')
+#while [ 1 ]; do
+i2cset -y 1 0x26 3
 
-if [ $btcurrent = $notconnected ]; then
-        echo "Attempting connection..."
-        rfcomm connect 0 $device 1 2> /dev/null >/dev/null &
-        sleep 1
-fi
+cmdout=$(l2ping $device -c 1)
+btcurrent=$(echo $cmdout | grep -c "1 sent") 2> /dev/null
+
+echo $btcurrent
+i2cset -y 1 0x26 0
 
 if [ $btcurrent = $connected ]; then
-        echo "Device connected. RSSI: "$rssi
+    i2cset -y 1 0x26 1
 fi
-
-if [ $btconnected -ne $btcurrent ]; then
-        if [ $btcurrent -eq 0 ]; then
-                echo "GONE!"
-        fi
-        if [ $btcurrent -eq 1 ]; then
-                echo "HERE!"
-        fi
-        btconnected=$btcurrent
-fi
-
 sleep 1
 
-done
+i2cset -y 1 0x26 0
+
+#rssi=$(echo $cmdout | sed -e 's/RSSI return value: //g')
+
+#if [ $btcurrent = $notconnected ]; then
+#        echo "Attempting connection..."
+#        rfcomm connect 0 $device 1 2> /dev/null >/dev/null &
+#        sleep 1
+#fi
+
+#if [ $btcurrent = $connected ]; then
+#        echo "Device connected. RSSI: "$rssi
+#fi
+
+#if [ $btconnected -ne $btcurrent ]; then
+#        if [ $btcurrent -eq 0 ]; then
+#                echo "GONE!"
+#        fi
+#        if [ $btcurrent -eq 1 ]; then
+#                echo "HERE!"
+#        fi
+#        btconnected=$btcurrent
+#fi
+
+#sleep 1
+
+#done

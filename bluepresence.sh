1db8729fd64731b2e614cccd4c2f69ea51dced8d
Jonathan Green <jonathan.green@wunelli.co.uk>
2014-12-05 11:58:24 +0000

    adding proper screen dims

---
+++ bluepresence.sh
@@ -0,0 +1,39 @@
+#Global VARS:
+device="C0:63:94:4E:62:F3"
+btconnected=0
+btcurrent=-1
+counter=0
+notconnected="0"
+connected="1"
+rssi=-1
+state=0
+
+#Command loop:
+while [ 1 ]; do
+cmdout=$(l2ping $device -c 1)
+btcurrent=$(echo $cmdout | grep -c "RSSI return value") 2> /dev/null
+rssi=$(echo $cmdout | sed -e 's/RSSI return value: //g')
+
+if [ $btcurrent = $notconnected ]; then
+        echo "Attempting connection..."
+        rfcomm connect 0 $device 1 2> /dev/null >/dev/null &
+        sleep 1
+fi
+
+if [ $btcurrent = $connected ]; then
+        echo "Device connected. RSSI: "$rssi
+fi
+
+if [ $btconnected -ne $btcurrent ]; then
+        if [ $btcurrent -eq 0 ]; then
+                echo "GONE!"
+        fi
+        if [ $btcurrent -eq 1 ]; then
+                echo "HERE!"
+        fi
+        btconnected=$btcurrent
+fi
+
+sleep 1
+
+done

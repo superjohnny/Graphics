46a1da95e4d5de8c50e105a1c19a5bd4f3f502b3
Jonathan Green <jonathan.green@wunelli.co.uk>
2014-12-09 17:12:53 +0000

    date test with method

--- datetest.py
+++ datetest.py
@@ -9,10 +9,16 @@ from datetime import datetime
 from datetime import timedelta
 
 
+def Lights(theValue):
+    return theValue < 5
+
+
 t = datetime.now()
 
 for x in range(0,240):
     #print "value %d" % (x)
     d = datetime.now() - t
-    print d.seconds
+    print "the value %d and %d" % (d.seconds, Lights(d.seconds))
     sleep(1)
+
+
